02d7ec331df7253c92b3324c28213fd2643ba17f
Tajah Johnson <tajah93@hotmail.com>
2020-12-02 00:18:13 -0600

    Fixed syntax error

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import routes from './routes';
@@ -6,10 +5,7 @@ import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  routes, document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
